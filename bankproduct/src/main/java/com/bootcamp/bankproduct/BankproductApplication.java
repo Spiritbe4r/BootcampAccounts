2158f2822478a473622c2a9f81a6180e5d657797
pwhuanco <pedrohuanco@gmail.com>
2022-03-08 22:22:44 -0500

    primera subida

---
+++ bankproduct/src/main/java/com/bootcamp/bankproduct/BankproductApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankproductApplication.class, args);
+	}
+
+}
