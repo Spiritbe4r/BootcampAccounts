7592444ad1f77ab0a0a2fabb6492495921dac6ba
pwhuanco <pedrohuanco@gmail.com>
2022-03-10 00:11:27 -0500

    configuracion y uso de Logback

--- bankproduct/src/main/java/com/bootcamp/bankproduct/controller/AccountController.java
+++ bankproduct/src/main/java/com/bootcamp/bankproduct/controller/AccountController.java
@@ -2,6 +2,8 @@ package com.bootcamp.bankproduct.controller;
 
 import com.bootcamp.bankproduct.dto.AccountDto;
 import com.bootcamp.bankproduct.service.AccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,14 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping(path = "/api/accounts")
 public class AccountController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountController.class);
+
     @Autowired
     private AccountService accountService;
 
     @GetMapping
     public Flux<AccountDto> getAccount(){
+        LOGGER.debug("Getting clients!");
         return accountService.getClients();
     }
 }
