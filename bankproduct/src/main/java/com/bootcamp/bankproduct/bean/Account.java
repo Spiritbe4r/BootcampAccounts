3756ffc343e42bff188886536e6c85b3206f3635
Spiritbe4r <flywithmee20@gmail.com>
2022-03-10 21:55:19 -0500

    ra

--- bankproduct/src/main/java/com/bootcamp/bankproduct/bean/Account.java
+++ bankproduct/src/main/java/com/bootcamp/bankproduct/bean/Account.java
@@ -1,5 +1,6 @@
 package com.bootcamp.bankproduct.bean;
 
+import com.bootcamp.bankproduct.dto.ClientDto;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,11 +10,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Account {
 	@Id
 	private String id;
-	private String balance;
+	private String amount;
 	private String currency;
-	private String idClient;
+
 	private String typeSavingAcc;
 	private String typeCurrentAcc;
-	private String typeTermAcc;
+
+	private ClientDto clientDto;
 
 }
---
+++ bankproduct/src/main/java/com/bootcamp/bankproduct/dto/ClientDto.java
@@ -0,0 +1,7 @@
+package com.bootcamp.bankproduct.dto;
+
+public class ClientDto {
+    private String id;
+    private String idClient;
+    private String email;
+}
