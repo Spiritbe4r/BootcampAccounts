12f596c5e505968c8c4c4d1de47d0e95a40a97d3
Ronal <66176445+Spiritbe4r@users.noreply.github.com>
2022-03-11 20:40:07 -0500

    .

--- reactive/src/main/java/com/cardenascode/reactive/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cardenascode.reactive.repository;
-
-import com.cardenascode.reactive.model.Employee;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface EmployeeRepository extends ReactiveMongoRepository<Employee,String> {
-}
